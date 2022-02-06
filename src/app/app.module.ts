948622dcb4cee2d0ec498c1b84296ee2d4191304
michael-muga <mugamichaelshawn@gmail.com>
2022-02-06 22:06:28 +0300

    vote component added to the ngmodule declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { CreateQuoteComponent } from './create-quote/create-quote.component';
+import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    CreateQuoteComponent
+    CreateQuoteComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule,
