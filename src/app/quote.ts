54b6adccc1d23160c06c75adc8e520529deb50ff
michael-muga <mugamichaelshawn@gmail.com>
2022-02-06 22:10:35 +0300

    show detail property added to the quote module class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,6 @@
 export class Quote {
+    showDetails: boolean;
     constructor(public id: number, public quote: string, public author: string, public submitter: string){
+        this.showDetails=false;
     }
 }
